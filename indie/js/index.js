1925c4c070a5195cca956a32d84c595e6562964d
Jack Feng <fdy409784@outlook.com>
2019-06-27 00:51:28 +0800

    init

---
+++ indie/js/index.js
@@ -0,0 +1,46 @@
+;(function(){
+
+    jQuery(document).ready(function ($) {
+        realTime();
+        window.realTimeTimer = setInterval(realTime, 500);
+    });
+
+    function realTime(){
+        let d = new Date();
+        let timeDom = document.querySelector('#realTime');
+        timeDom.innerHTML = d.toLocaleString();
+    }
+
+}());
+
+;(function(){
+
+    function createGame(){
+        
+        var game = new gameInit();
+        return game;
+    }
+
+    function gameInit(){
+        this.user={
+            power:0,
+            status:'alive',
+        }
+    }
+    gameInit.prototype = {
+        constructor: gameInit,
+        userFunc: {
+            addPower(num = 1) {
+                this.user.power += num;
+                return this;
+            },
+        },
+        getStatus(){
+            return this.user.status;
+        }
+        
+    }
+
+    window.createGame = createGame;
+
+}());
